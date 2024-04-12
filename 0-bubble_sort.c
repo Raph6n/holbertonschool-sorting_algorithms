759e9a9752476a3edf8f48a40fe2f2ba3abf1de8
Raph6n <naim.raphael@outlook.fr>
2024-04-12 12:05:07 +0000

    task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,12 +10,12 @@
 
 void bubble_sort(int *array, size_t size)
 {
-if (array == NULL || size < 2)
-return;
-
 size_t count_1, count_2;
 int transfert;
 
+if (array == 0 || size < 2)
+return;
+ 
 for (count_1 = 0; count_1 < size - 1; count_1++)
 {
 for (count_2 = 0; count_2 < size - count_1 - 1; count_2++)
--- sort.h
+++ sort.h
@@ -11,7 +11,7 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
-
+void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
