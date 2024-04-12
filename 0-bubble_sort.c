16992364cdfd5f4aad9d8b7a1e41b69f515c61b7
Raph6n <naim.raphael@outlook.fr>
2024-04-12 11:54:25 +0000

    task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,31 @@
 #include "sort.h"
+#include <stdio.h>
+
+/**
+ * bubble_sort - fction switch element
+ * @array: var tab
+ * @size: var
+ */
+
 
 void bubble_sort(int *array, size_t size)
 {
-  
+if (array == NULL || size < 2)
+return;
+
+size_t count_1, count_2;
+int transfert;
+
+for (count_1 = 0; count_1 < size - 1; count_1++)
+{
+for (count_2 = 0; count_2 < size - count_1 - 1; count_2++)
+{
+if (array[count_2] > array[count_2 + 1])
+{
+transfert = array[count_2];
+array[count_2] = array[count_2 + 1];
+array[count_2 + 1] = transfert;
+}
+}
+}
+}
--- sort.h
+++ sort.h
@@ -1,15 +1,9 @@
 #ifndef BIG_O
 #define BIG_O
-
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list);
-void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
-void print_list(const listint_t *list);
-oid print_array(const int *array, size_t size);
 
 typedef struct listint_s
 {
@@ -17,5 +11,13 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
+
+
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+
 
 #endif
