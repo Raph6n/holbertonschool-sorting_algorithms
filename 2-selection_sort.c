ddac794cc3d5fadd260c828db403d2a68086f0cb
Raph6n <naim.raphael@outlook.fr>
2024-04-13 19:41:19 +0000

    task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+void selection_sort(int *array, size_t size)
+{
+size_t count_1, count_2, min;
+int transfert;
+
+if (array == NULL || size < 2)
+return;
+
+for (count_1 = 0; count_1 < size - 1; count_1++)
+{
+min = count_1;
+for (count_2 = count_1 + 1; count_2 < size; count_2++)
+{
+if (array[count_2] < array[min])
+min = j;
+}
+if (min != count_1)
+{
+transfert = array[count_1];
+array[count_1] = array[min];
+array[min] = transfert;
+print_array(array, size);
+}
+}
+}
